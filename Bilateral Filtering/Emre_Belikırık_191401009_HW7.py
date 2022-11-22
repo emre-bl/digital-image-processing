24ad3e29d97ec831dbcfde27321bcb00b61eb2d2
ByAlu <emrebelikirik4664@gmail.com>
2022-11-22 15:55:46 +0300

    -bl td

--- Bilateral Filtering/Emre_Belikırık_191401009_HW7.py
+++ Bilateral Filtering/Emre_Belikırık_191401009_HW7.py
@@ -1,10 +1,10 @@
 import numpy as np
 import cv2 as cv
 
-print("----------------- 1st Question -----------------")
-
 lena_grayscale_hq = cv.imread("lena_grayscale_hq.jpg", cv.IMREAD_GRAYSCALE)
 
+print("------------------ 1st Question ------------------")
+ 
 def adaptive_mean_filter(image,gamma):
     img = image.copy()
     return_img = np.zeros(img.shape)
@@ -26,10 +26,13 @@ noisyImage_Gaussian = cv.imread("noisyImage_Gaussian.jpg", cv.IMREAD_GRAYSCALE)
 normalized_noisyImage_Gaussian = np.single(noisyImage_Gaussian)
 normalized_noisyImage_Gaussian = cv.normalize(normalized_noisyImage_Gaussian, None, alpha=0,beta=1, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F)
 
-b3 = cv.blur(normalized_noisyImage_Gaussian, (3,3), borderType=cv.BORDER_REPLICATE)
+
+b3 = None
+b3 = cv.boxFilter(normalized_noisyImage_Gaussian, 0, (3,3), b3, (-1,-1), True, cv.BORDER_REPLICATE)
 b3 = cv.normalize(b3, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F).astype(np.uint8)
 
-b5 = cv.blur(normalized_noisyImage_Gaussian, (5,5), borderType=cv.BORDER_REPLICATE)
+b5 = None
+b5 = cv.boxFilter(normalized_noisyImage_Gaussian, 0, (5,5), b5, (-1,-1), True, cv.BORDER_REPLICATE)
 b5 = cv.normalize(b5, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F).astype(np.uint8)
 
 g3 = cv.GaussianBlur(normalized_noisyImage_Gaussian,(3,3),0,borderType = cv.BORDER_REPLICATE)
@@ -44,33 +47,27 @@ bilateralfiltered = cv.bilateralFilter(normalized_noisyImage_Gaussian, 5, 3, 0.9
 bilateralfiltered = cv.normalize(bilateralfiltered, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F).astype(np.uint8)
 
 
-b3psnr = cv.PSNR(b3, lena_grayscale_hq)
-print("3x3 Box Filter PSNR: ",b3psnr)
-b5psnr = cv.PSNR(b5, lena_grayscale_hq)
-print("5x5 Box Filter PSNR:",b5psnr)
+print("3x3 Box Filter PSNR: ", cv.PSNR(b3, lena_grayscale_hq))
+print("5x5 Box Filter PSNR:",cv.PSNR(b5, lena_grayscale_hq))
+print("3x3 Gaussian PSNR:",cv.PSNR(g3, lena_grayscale_hq))
+print("5x5 Gaussian PSNR:",cv.PSNR(g5, lena_grayscale_hq))
+print("Adaptive mean filtered PSNR:",cv.PSNR(adap_mean_filtered, lena_grayscale_hq))
+print("Bilateral Filtered PSNR:",cv.PSNR(bilateralfiltered, lena_grayscale_hq))
 
-g3psnr = cv.PSNR(g3, lena_grayscale_hq)
-print("3x3 Gaussian PSNR:",g3psnr)
-g5psnr = cv.PSNR(g5, lena_grayscale_hq)
-print("5x5 Gaussian PSNR:",g5psnr)
-
-adap_mean_filtered_psnr = cv.PSNR(adap_mean_filtered, lena_grayscale_hq)
-print("Adaptive mean filtered PSNR:",adap_mean_filtered_psnr)
-
-bilateralfiltered_psnr = cv.PSNR(bilateralfiltered, lena_grayscale_hq)
-print("Bilateral Filtered PSNR:",bilateralfiltered_psnr)
-
-print("----------------- 2nd Question -----------------")
+#2. Soru
+print("------------------ 2nd Question ------------------")
 
 noisyImage_Gaussian_01 = cv.imread("noisyImage_Gaussian_01.jpg", cv.IMREAD_GRAYSCALE)
 
 normalized_noisyImage_Gaussian_01 = np.single(noisyImage_Gaussian_01)
 normalized_noisyImage_Gaussian_01 = cv.normalize(normalized_noisyImage_Gaussian_01, None, alpha=0,beta=1, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F)
 
-b3_01 = cv.blur(normalized_noisyImage_Gaussian_01, (3,3), borderType=cv.BORDER_REPLICATE)
+b3_01 = None
+b3_01 = cv.boxFilter(normalized_noisyImage_Gaussian_01, 0, (3,3), b3_01, (-1,-1), True, cv.BORDER_REPLICATE)
 b3_01 = cv.normalize(b3_01, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F).astype(np.uint8)
 
-b5_01 = cv.blur(normalized_noisyImage_Gaussian_01, (5,5), borderType=cv.BORDER_REPLICATE)
+b5_01 = None
+b5_01 = cv.boxFilter(normalized_noisyImage_Gaussian_01, 0, (5,5), b5_01, (-1,-1), True, cv.BORDER_REPLICATE)
 b5_01 = cv.normalize(b5_01, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F).astype(np.uint8)
 
 g3_01 = cv.GaussianBlur(normalized_noisyImage_Gaussian_01,(3,3),0,borderType = cv.BORDER_REPLICATE)
@@ -84,24 +81,16 @@ adap_mean_filtered_01 = adaptive_mean_filter(normalized_noisyImage_Gaussian_01,0
 bilateralfiltered_01 = cv.bilateralFilter(normalized_noisyImage_Gaussian_01, 3, 0.1, 1, borderType = cv.BORDER_REPLICATE)
 bilateralfiltered_01 = cv.normalize(bilateralfiltered_01, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F).astype(np.uint8)
 
+print("3x3 Box Filter PSNR: ",cv.PSNR(b3_01, lena_grayscale_hq))
+print("5x5 Box Filter PSNR:",cv.PSNR(b5_01, lena_grayscale_hq))
+print("3x3 Gaussian PSNR:",cv.PSNR(g3_01, lena_grayscale_hq))
+print("5x5 Gaussian PSNR:",cv.PSNR(g5_01, lena_grayscale_hq))
+print("Adaptive mean filtered PSNR:",cv.PSNR(adap_mean_filtered_01, lena_grayscale_hq))
+print("Bilateral Filtered PSNR:",cv.PSNR(bilateralfiltered_01, lena_grayscale_hq))
 
-b3_01psnr = cv.PSNR(b3_01, lena_grayscale_hq)
-print("3x3 Box Filter PSNR: ",b3_01psnr)
-b5_01psnr = cv.PSNR(b5_01, lena_grayscale_hq)
-print("5x5 Box Filter PSNR:",b5_01psnr)
 
-g3_01psnr = cv.PSNR(g3_01, lena_grayscale_hq)
-print("3x3 Gaussian PSNR:",g3_01psnr)
-g5_01psnr = cv.PSNR(g5_01, lena_grayscale_hq)
-print("5x5 Gaussian PSNR:",g5_01psnr)
-
-adap_mean_filtered_01_psnr = cv.PSNR(adap_mean_filtered_01, lena_grayscale_hq)
-print("Adaptive mean filtered PSNR:",adap_mean_filtered_01_psnr)
-
-bilateralfiltered_01_psnr = cv.PSNR(bilateralfiltered_01, lena_grayscale_hq)
-print("Bilateral Filtered PSNR:",bilateralfiltered_01_psnr)
-
-print("----------------- 3rd Question -----------------")
+#3. Soru
+print("------------------ 3rd Question ------------------")
 
 
 def G_x(x,sigma):
@@ -119,7 +108,6 @@ def kernel_calculation(part,sigma_r,sigma_s):
             normalization+=(G_x(distance,sigma_s)*G_x(intensity_difference,sigma_r))
             sum += G_x(distance,sigma_s)*G_x(intensity_difference,sigma_r)*part[i,j]
     return sum/normalization
-
 
 def bilateral_filter(image,kernel_size,sigma_s,sigma_r):
     border = kernel_size//2
@@ -143,32 +131,40 @@ noisyImage_Gaussian = cv.imread("noisyImage_Gaussian.jpg", cv.IMREAD_GRAYSCALE)
 normalized_noisyImage_Gaussian = np.single(noisyImage_Gaussian)
 normalized_noisyImage_Gaussian = cv.normalize(normalized_noisyImage_Gaussian, None, alpha=0,beta=1, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F)
 
+cv_output_01 = cv.bilateralFilter(normalized_noisyImage_Gaussian_01, 5, 0.1, 1, borderType = cv.BORDER_REPLICATE)
+cv_output_01 = cv.normalize(cv_output_01, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F)
 
-cv1 = cv.bilateralFilter(normalized_noisyImage_Gaussian_01, 5, 0.1, 1, borderType = cv.BORDER_REPLICATE)
-cv1 = cv.normalize(cv1, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F)
+cv_output = cv.bilateralFilter(normalized_noisyImage_Gaussian, 5, 3, 0.9, borderType = cv.BORDER_REPLICATE)
+cv_output = cv.normalize(cv_output, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F)
 
-cv2 = cv.bilateralFilter(normalized_noisyImage_Gaussian, 5, 3, 0.9, borderType = cv.BORDER_REPLICATE)
-cv2 = cv.normalize(cv2, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F)
+my_output_01 = bilateral_filter(normalized_noisyImage_Gaussian_01,5, 0.1, 1)
+my_output_01 = cv.normalize(my_output_01, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F)
 
-mine1 = bilateral_filter(normalized_noisyImage_Gaussian_01,5, 0.1, 1)
-mine1 = cv.normalize(mine1, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F)
+my_output = bilateral_filter(normalized_noisyImage_Gaussian,5, 3, 0.9)
+my_output = cv.normalize(my_output, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F)
 
-mine2 = bilateral_filter(normalized_noisyImage_Gaussian,5, 3, 0.9)
-mine2 = cv.normalize(mine2, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F)
 
-cv.imshow("Bilateral filtered noisyImage_Gaussian_01 output(OPENCV)",mine1.astype(np.uint8))
+cv.imshow("Bilateral filtered noisyImage_Gaussian_01 output(OpenCV)",cv_output_01.astype(np.uint8))
 
-cv.imshow("Bilateral filtered noisyImage_Gaussian_01 output(My Filter)",mine1.astype(np.uint8))
+cv.imshow("Bilateral filtered noisyImage_Gaussian_01 output(My Filter)",my_output_01.astype(np.uint8))
 
-cv.imshow("Bilateral filtered noisyImage_Gaussian output(OPENCV)",mine1.astype(np.uint8))
+cv.imshow("Bilateral filtered noisyImage_Gaussian output(OpenCV)",cv_output.astype(np.uint8))
 
-cv.imshow("Bilateral filtered noisyImage_Gaussian output(My Filter)",mine1.astype(np.uint8))
+cv.imshow("Bilateral filtered noisyImage_Gaussian output(My Filter)",my_output.astype(np.uint8))
 
 cv.waitKey(0)
 cv.destroyAllWindows()
 
-abs_img1 = abs(mine1.astype(float)-cv1.astype(float))
-print("[noisyImage_Gaussian_01] Max pixel intensity difference:",round(abs_img1.max()))
+abs_img_01 = abs(my_output_01.astype(float)-cv_output_01.astype(float))
+print("[noisyImage_Gaussian_01] Max pixel intensity difference:",round(abs_img_01.max()))
 
-abs_img2 = abs(mine2.astype(float)-cv2.astype(float))
-print("[noisyImage_Gaussian] Max pixel intensity difference:",round(abs_img2.max()))
+
+abs_img = abs(my_output.astype(float)-cv_output.astype(float))
+print("[noisyImage_Gaussian] Max pixel intensity difference:",round(abs_img.max()))
+
+
+print("For (noisyImage_Gaussian) my bilateral filter PSNR:",cv.PSNR(my_output.astype(np.uint8), lena_grayscale_hq))
+print("For (noisyImage_Gaussian) OpenCV PSNR:",cv.PSNR(cv_output.astype(np.uint8), lena_grayscale_hq))
+
+print("For (noisyImage_Gaussian_01) my bilateral filter PSNR:",cv.PSNR(my_output_01.astype(np.uint8), lena_grayscale_hq))
+print("For (noisyImage_Gaussian_01) OpenCV PSNR:",cv.PSNR(cv_output_01.astype(np.uint8), lena_grayscale_hq))
